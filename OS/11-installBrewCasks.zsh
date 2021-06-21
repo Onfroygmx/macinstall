dd7c2d6a433f618bb683d916122be34e7c9c2b88
onf_mbp <onfroy@gmx.com>
2021-06-21 11:05:09 +0200

    Organized and added caks

--- OS/11-installBrewCasks.zsh
+++ OS/11-installBrewCasks.zsh
@@ -1,3 +1,36 @@
 #! /usr/bin/env zsh
 
-### Install base home brew casks
+### Install home brew casks
+
+## Utilities
+brew install --cask appcleaner
+brew install --cask bitwarden
+brew install --cask easyfind
+brew install --cask etrecheckpro
+brew install --cask sourcetree
+brew install --cask thunderbird
+brew install --cask transmission
+brew install --cask virtualbox
+brew install --cask virtualbox-extension-pack
+brew install --cask vlc
+
+## Editors
+
+brew install --cask \
+    atom \
+    sublime-text \
+    visual-studio-code
+
+## Browsers
+
+brew install --cask \
+    brave \
+    firefox \
+    tor \
+    opera
+
+brew cleanup -s
+brew cask cleanup
+#now diagnotic
+brew doctor
+brew missing
